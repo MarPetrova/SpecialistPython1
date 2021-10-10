92e4967209f856276e50ae9f22b098eef68d3614
MarPetrova <91017967+MarPetrova@users.noreply.github.com>
2021-10-10 12:36:39 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,13 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def hh_mm_ss(sec):
+    hh=sec//60**2
+    mm=sec//60-hh*60
+    ss=sec-hh*60*60-mm*60
+    #return f"{hh}:{mm}:{ss}"
+    return f"{hh:0>2}:{mm:0>2}:{ss:0>2}"
+print(hh_mm_ss(29143))
+print(hh_mm_ss(300))
+print(hh_mm_ss(58))
